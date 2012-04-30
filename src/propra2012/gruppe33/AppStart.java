b1e10eff757eb6a6c25d83e0e008e05a49f817e7
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2012-04-30 17:39:41 +0200

    src folder added

---
+++ src/propra2012/gruppe33/AppStart.java
@@ -0,0 +1,16 @@
+package propra2012.gruppe33;
+
+/**
+ * 
+ * @author Christopher Probst
+ */
+public class AppStart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
