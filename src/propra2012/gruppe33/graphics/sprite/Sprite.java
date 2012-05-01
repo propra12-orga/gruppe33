c330ec9d35b14910775c0dd94115010edeb3ec22
Matthias Hesse <matthias.hesse@uni-duesseldorf.de>
2012-05-01 12:20:44 +0200

    Sprite Classes Created

---
+++ src/propra2012/gruppe33/graphics/sprite/Sprite.java
@@ -0,0 +1,8 @@
+package propra2012.gruppe33.graphics.sprite;
+
+import java.awt.image.BufferedImage;
+
+public interface Sprite {
+
+	BufferedImage getImage(String name);
+}
---
+++ src/propra2012/gruppe33/graphics/sprite/SpriteImpl.java
@@ -0,0 +1,19 @@
+package propra2012.gruppe33.graphics.sprite;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteImpl implements Sprite {
+
+	
+	
+	
+	@Override
+	public BufferedImage getImage(String name) {
+		
+		
+		
+		
+		return null;
+	}
+
+}
