91a7359d09e03bdba46dc3e024529d6dcf577107
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2012-05-01 17:35:05 +0200

    erste implementierung von load in Maploader

--- src/Map/Map.java
+++ src/Map/Map.java
@@ -1,5 +1,14 @@
 package Map;
 
 public class Map {
+	private String name;
+	//private char[][] grid;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- src/Map/MapLoader.java
+++ src/Map/MapLoader.java
@@ -1,5 +1,35 @@
 package Map;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*   
+ *Diese Klasse lädt aus einer Textdatei Zeilenweise die Karte aus.
+ *Kann IOExceptions werfen. 
+ */
 public class MapLoader {
 
+	/*
+	 * Berechnet die Map aus der Textdatei und gibt ein Array mit dem Inhalt der
+	 * Map zurück. Wirft IOException, wenn die Karte nicht geladen werden kann,
+	 * z.B. wenn der File nicht geladen werden kann.
+	 */
+	public char[][] load() throws IOException {
+		String line; // Eine Zeile der Karte.
+
+		// Laden des files
+		BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+		
+		//Initialisieren des Arrays fehlt.
+		
+		line = br.readLine();
+		while (line != null) {
+			for (int i = 0; i < line.length()-1; i++) {
+				//line.charAt(i)= load
+			}
+		}
+		br.close();
+		return load();
+	}
 }
