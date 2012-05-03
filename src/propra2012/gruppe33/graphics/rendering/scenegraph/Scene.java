092f99e906698570f2d213f640df22936a8e6ecb
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2012-05-03 14:10:30 +0200

    scene class doc completed

--- src/propra2012/gruppe33/graphics/rendering/scenegraph/Scene.java
+++ src/propra2012/gruppe33/graphics/rendering/scenegraph/Scene.java
@@ -9,9 +9,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * A scene is a special entity which has some more features:
+ * 
+ * - Keyboard input management
+ * 
+ * - Scaled rendering (always chooses the maximum size but never ignores the
+ * ratio!!!) This is very handy.
+ * 
+ * Since this is also an entity you have all basic features already provided by
+ * the entity class. Obviously you can add multiple entities to a scene :-) .
  * 
  * @author Christopher Probst
- * 
+ * @see Entity
+ * @see Vector2f
  */
 public class Scene extends Entity implements KeyListener {
 
@@ -21,40 +31,91 @@ public class Scene extends Entity implements KeyListener {
 	// The width and height of the scene
 	private int width, height;
 
-	public Scene(String id, int width, int height) {
-		super(id);
+	/**
+	 * Creates a new scene.
+	 * 
+	 * @param name
+	 *            The name of the scene.
+	 * @param width
+	 *            The provided viewport width.
+	 * @param height
+	 *            The provided viewport height.
+	 */
+	public Scene(String name, int width, int height) {
+		super(name);
 		setWidth(width);
 		setHeight(height);
 	}
 
+	/**
+	 * Clears all keyboard states.
+	 */
 	public void clearKeyboardState() {
 		// Clear the complete keyboard state
 		keyboardState.clear();
 	}
 
+	/**
+	 * Returns a given key's pressed status.
+	 * 
+	 * @param vk
+	 *            The keycode you want to check. (KeyEvent.VK_****)
+	 * @return true if the key is pressed otherwise false.
+	 */
 	public boolean isPressed(int vk) {
 		Boolean pressed = keyboardState.get(vk);
 		return pressed != null ? pressed : false;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.KeyListener#keyPressed(java.awt.event.KeyEvent)
+	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
 		keyboardState.put(e.getKeyCode(), Boolean.TRUE);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.KeyListener#keyReleased(java.awt.event.KeyEvent)
+	 */
 	@Override
 	public void keyReleased(KeyEvent e) {
 		keyboardState.put(e.getKeyCode(), Boolean.FALSE);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.KeyListener#keyTyped(java.awt.event.KeyEvent)
+	 */
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
 
+	/**
+	 * @return the x-y ratio as float.
+	 */
+	public float getRatio() {
+		return width / (float) height;
+	}
+
+	/**
+	 * @return the viewport width.
+	 */
 	public int getWidth() {
 		return width;
 	}
 
+	/**
+	 * Sets the viewport width.
+	 * 
+	 * @param width
+	 *            The new width of the viewport.
+	 */
 	public void setWidth(int width) {
 		if (width <= 0) {
 			throw new IllegalArgumentException("width must be > 0");
@@ -63,10 +124,19 @@ public class Scene extends Entity implements KeyListener {
 		this.width = width;
 	}
 
+	/**
+	 * @return the viewport height.
+	 */
 	public int getHeight() {
 		return height;
 	}
 
+	/**
+	 * Sets the viewport height.
+	 * 
+	 * @param height
+	 *            The new height of the viewport.
+	 */
 	public void setHeight(int height) {
 		if (height <= 0) {
 			throw new IllegalArgumentException("height must be > 0");
@@ -75,11 +145,38 @@ public class Scene extends Entity implements KeyListener {
 		this.height = height;
 	}
 
-	public void simulate(Graphics g, int w, int h) {
-		simulate(g, w, h, 0);
+	/**
+	 * Simulates the scene with a time-per-frame of 0. This method will
+	 * transforms the rendering in a way, that the viewport of the scene is
+	 * translated to the destination frame (on what you are drawing). Basically
+	 * needed for simple rendering without modifying the states of the children.
+	 * 
+	 * @param graphics
+	 *            The graphics context you want to render to.
+	 * @param width
+	 *            The width of the destination frame.
+	 * @param height
+	 *            The height of the destination frame.
+	 */
+	public void simulate(Graphics graphics, int width, int height) {
+		simulate(graphics, width, height, 0);
 	}
 
-	public void simulate(Graphics g, int w, int h, long tpf) {
+	/**
+	 * Simulates the scene with a given time-per-frame. This method will
+	 * transforms the rendering in a way, that the viewport of the scene is
+	 * translated to the destination frame (on what you are drawing).
+	 * 
+	 * @param graphics
+	 *            The graphics context you want to render to.
+	 * @param width
+	 *            The width of the destination frame.
+	 * @param height
+	 *            The height of the destination frame.
+	 * @param tpf
+	 *            The time-per-frame to update the children states.
+	 */
+	public void simulate(Graphics graphics, int width, int height, long tpf) {
 
 		// Only update if tpf is > 0
 		if (tpf > 0) {
@@ -89,41 +186,42 @@ public class Scene extends Entity implements KeyListener {
 		}
 
 		// Not visible...
-		if (w <= 0 || h <= 0) {
+		if (width <= 0 || height <= 0) {
 			return;
 		}
 
 		// Convert
-		Graphics2D g2d = (Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) graphics;
 
 		// Calc the ratio which we have to fit
-		float dstRatio = width / (float) height;
+		float dstRatio = getRatio();
 
 		// Calc new dimension
-		float newWidth = w, newHeight = w / dstRatio;
+		float newWidth = width, newHeight = width / dstRatio;
 
 		// Check height
-		if (newHeight > h) {
+		if (newHeight > height) {
 
 			// Recalc
-			newHeight = h;
+			newHeight = height;
 			newWidth = dstRatio * newHeight;
 		}
 
 		// At first translate the context
-		g2d.translate(w * 0.5f - newWidth * 0.5f, h * 0.5f - newHeight * 0.5f);
+		g2d.translate(width * 0.5f - newWidth * 0.5f, height * 0.5f - newHeight
+				* 0.5f);
 
 		// Scale using the vector
-		g2d.scale(newWidth / width, newHeight / height);
+		g2d.scale(newWidth / this.width, newHeight / this.height);
 
 		// Limit the clip
-		g2d.setClip(0, 0, width, height);
+		g2d.setClip(0, 0, this.width, this.height);
 
 		// Set background color
 		g2d.setBackground(Color.white);
 
 		// Clear the rect
-		g2d.clearRect(0, 0, width, height);
+		g2d.clearRect(0, 0, this.width, this.height);
 
 		// Finally render
 		render(g2d);
