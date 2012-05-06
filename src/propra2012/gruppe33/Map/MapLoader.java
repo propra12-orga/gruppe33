2b901c4097a538860231c3148bc27927c7341145
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2012-05-06 14:40:04 +0200

    Grid_behaviour branch finished

--- src/propra2012/gruppe33/Map/MapLoader.java
+++ src/propra2012/gruppe33/Map/MapLoader.java
@@ -1,8 +1,9 @@
-package propra2012.gruppe33.Map;
+package propra2012.gruppe33.map;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
