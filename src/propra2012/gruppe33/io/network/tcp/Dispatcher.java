da20057bd410c1a51bc76456fd215d2593530ddf
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2012-05-11 19:00:48 +0200

    Switched to Netty3.x Custom NIO impl would take too much time..

--- src/propra2012/gruppe33/io/network/tcp/Dispatcher.java
+++
@@ -1,9 +0,0 @@
-package propra2012.gruppe33.io.network.tcp;
-
-import java.io.IOException;
-import java.nio.channels.SelectionKey;
-
-public interface Dispatcher {
-
-	void dispatch(SelectionKey key) throws IOException;
-}
