8f49eb8dcc788d4d2555b96ebfa13bac9b0bff5f
Matthias Hesse <matthias.hesse@uni-duesseldorf.de>
2012-05-30 18:57:27 +0200

    Removed not needed files

---
+++ src/propra2012/gruppe33/sound/ExtendedSoundEngine.java
@@ -0,0 +1,19 @@
+package propra2012.gruppe33.sound;
+
+import java.net.URL;
+import java.util.Map;
+
+/**
+ * 
+ * @author Matthias Hesse/MrBumble
+ *
+ */
+
+public class ExtendedSoundEngine {
+	private Map<String, URL> soundMap;
+
+	public ExtendedSoundEngine() {
+
+	}
+
+}
