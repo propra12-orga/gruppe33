93c1fcd602971cbedacdd8633500e54b1492e4ea
Matthias Hesse <matthias.hesse@uni-duesseldorf.de>
2012-05-31 17:28:27 +0200

    Fixing some Sound Issues

--- src/propra2012/gruppe33/engine/sound/ExtendedSoundEngine.java
+++ src/propra2012/gruppe33/engine/sound/ExtendedSoundEngine.java
@@ -1,19 +1,17 @@
-package propra2012.gruppe33.sound;
+package propra2012.gruppe33.engine.sound;
 
-import java.applet.AudioClip;
-import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.io.InputStream;
 import java.util.Map;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
@@ -25,21 +23,15 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class ExtendedSoundEngine {
 
-
-
 	private static SourceDataLine audioLine;
 
 	private AudioFormat format;
 
 	private Map<String, AudioInputStream> soundMap;
 
-
-
-	public ExtendedSoundEngine(){
+	public ExtendedSoundEngine() {
 
 	}
-
-
 
 	public void addSound(String name, FileInputStream file)
 			throws UnsupportedAudioFileException, IOException {
@@ -51,41 +43,41 @@ public class ExtendedSoundEngine {
 		}
 	}
 
-
-
-
-
 	public void playSound() {
 
-	
 	}
 
 	public static void main(String[] agrs) throws Exception {
 
-		byte[] data = null;
+		InputStream input = new FileInputStream(new File("c:/back.wav"));
+
+		// File einlesen
+		ByteArrayOutputStream audioFile = new ByteArrayOutputStream();
+		byte[] copyBuffer = new byte[8192];
+		int read;
+		while ((read = input.read(copyBuffer)) != -1) {
+			audioFile.write(copyBuffer, 0, read);
+		}
+		input.close();
+		byte[] AUDIO_DATA = audioFile.toByteArray();
+
 		AudioInputStream ais = AudioSystem
-				.getAudioInputStream(new BufferedInputStream(
-						new FileInputStream(new File("c:/back.wav"))));
+				.getAudioInputStream(new ByteArrayInputStream(AUDIO_DATA));
 
-
-		
 		Clip clip = AudioSystem.getClip();
-		
+
 		clip.open(ais);
 		clip.start();
-		
+
 		Thread.sleep(1000);
-		
+
 		Clip clip2 = AudioSystem.getClip();
 
-		clip2.open(AudioSystem
-				.getAudioInputStream(new BufferedInputStream(
-						new FileInputStream(new File("c:/back.wav")))));
+		clip2.open(AudioSystem.getAudioInputStream(new ByteArrayInputStream(
+				AUDIO_DATA)));
 		clip2.start();
-		
+
 		Thread.sleep(222222);
-		
-		
 
 	}
 }
