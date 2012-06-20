6695e0db2ba670045b411be2c05b71dec5d5e348
Malte Schmidt <malte.schmidt@uni-duesseldorf.de>
2012-06-20 21:24:24 +0200

    finished dijkstra

--- src/propra2012/gruppe33/bomberman/ai/ninja/Bot.java
+++ src/propra2012/gruppe33/bomberman/ai/ninja/Bot.java
@@ -2,17 +2,77 @@ package propra2012.gruppe33.bomberman.ai.ninja;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import propra2012.gruppe33.bomberman.ai.AIControl;
 import propra2012.gruppe33.bomberman.ai.AIProcessor;
 
 public class Bot implements AIProcessor {
 
-	//private Pathfinder[][] map;
-	
+	private Pathfinder[][] map;
+	private boolean initialized;
+	private LinkedList<Pathfinder> path;
+
 	@Override
 	public void process(float timeSinceLastFrame, AIControl aiControl) {
 
+		if (!initialized) {
+			map = new Pathfinder[aiControl.fields().length - 1][aiControl
+					.fields()[0].length - 1];
+			initialize(aiControl.fields());
+			initialized = true;
+			for (int y = 0; y < map.length; y++) {
+				for (int x = 0; x < map.length; x++) {
+					if (aiControl.fields()[y][x] != null) {
+						map[y][x] = new Pathfinder();
+					}
+				}
+			}
+			for (int y = 0; y < map.length; y++) {
+				for (int x = 0; x < map[0].length; x++) {
+					if ((y - 1 >= 0) && (map[y - 1][x] != null)) {
+						map[y][x].setNorth(map[y - 1][x]);
+						map[y][x].setSpeedNorth(aiControl.edgeWeight(map[y][x],
+								map[y - 1][x]));
+					} else {
+						map[y][x].setNorth(map[y][x]);
+					}
+					if ((x + 1 < map[y].length) && (map[y][x + 1] != null)) {
+						map[y][x].setEast(map[y][x + 1]);
+						map[y][x].setSpeedEast(aiControl.edgeWeight(map[y][x],
+								map[y][x + 1]));
+					} else {
+						map[y][x].setEast(map[y][x]);
+					}
+					if ((y + 1 < map.length) && (map[y + 1][x] != null)) {
+						map[y][x].setSouth(map[y + 1][x]);
+						map[y][x].setSpeedSouth(aiControl.edgeWeight(map[y][x],
+								map[y + 1][x]));
+					} else {
+						map[y][x].setSouth(map[y][x]);
+					}
+					if ((x - 1 >= 0) && (map[y][x - 1] != null)) {
+						map[y][x].setWest(map[y][x - 1]);
+						map[y][x].setSpeedWest(aiControl.edgeWeight(map[y][x],
+								map[y][x - 1]));
+					} else {
+						map[y][x].setWest(map[y][x]);
+					}
+
+				}
+			}
+		}
+
+	}
+
+	public void initialize(int[][][] fields) {
+		for (int y = 0; y < fields.length; y++) {
+			for (int x = 0; x < fields[0].length; x++) {
+				if (fields[y][x] != null) {
+					// map[y][x] = new Pathfinder;
+				}
+			}
+		}
 	}
 
 	private void pathfind(Point[][] field, Point start, Point goal) {
@@ -29,7 +89,9 @@ public class Bot implements AIProcessor {
 
 	}
 	
-	private void dijkstra(Point[][] field, Point start, ArrayList<Pathfinder> crowd) {
+	private LinkedList getPath() {
+		path = new LinkedList<Pathfinder>();
 		
+		return path;
 	}
 }
--- src/propra2012/gruppe33/bomberman/ai/ninja/Pathfinder.java
+++ src/propra2012/gruppe33/bomberman/ai/ninja/Pathfinder.java
@@ -3,15 +3,81 @@ package propra2012.gruppe33.bomberman.ai.ninja;
 import java.awt.Point;
 
 public class Pathfinder {
-	private Point predecessor;
+	private Pathfinder predecessor;
 	private int distance;
 	private boolean visited;
+	private Pathfinder north;
+	private int speedNorth;
+	private Pathfinder east;
+	private int speedEast;
+	private Pathfinder south;
+	private int speedSouth;
+	private Pathfinder west;
+	private int speedWest;
+	private char indicator;
 
-	public Point getPredecessor() {
+	public Pathfinder() {
+		this.distance = 1000;
+	}
+
+	public Pathfinder lowestUncheckedConnection() {
+		if ((north != this) && (!north.isVisited())
+				&& (speedNorth <= speedEast) && (speedNorth <= speedSouth)
+				&& (speedNorth <= speedWest)) {
+			indicator = 'n';
+			return north;
+		} else if ((east != this) && (!east.isVisited())
+				&& (speedEast <= speedNorth) && (speedEast <= speedSouth)
+				&& (speedEast <= speedWest)) {
+			indicator = 'e';
+			return east;
+		} else if ((south != this) && (!south.isVisited())
+				&& (speedSouth <= speedNorth) && (speedSouth <= speedEast)
+				&& (speedSouth <= speedWest)) {
+			indicator = 's';
+			return south;
+		} else if ((west != this) && (!west.isVisited())
+				&& (speedWest <= speedNorth) && (speedWest <= speedEast)
+				&& (speedWest <= speedSouth)) {
+			indicator = 'w';
+			return west;
+		} else
+			return this;
+	}
+
+	public void dijkstra(int wayTillHere) {
+		Pathfinder next = this.lowestUncheckedConnection();
+		int speed = 0;
+		while (next != this) {
+			switch (indicator) {
+			case 'n':
+				speed = wayTillHere + speedNorth;
+				break;
+			case 'e':
+				speed = wayTillHere + speedEast;
+				break;
+			case 's':
+				speed = wayTillHere + speedSouth;
+				break;
+			case 'w':
+				speed = wayTillHere + speedWest;
+				break;
+			}
+			if (next.getDistance() > speed) {
+				next.setDistance(speed);
+				next.setPredecessor(this);
+			}
+			next.dijkstra(speed);
+			next = this.lowestUncheckedConnection();
+		}
+		visited = true;
+	}
+
+	public Pathfinder getPredecessor() {
 		return predecessor;
 	}
 
-	public void setPredecessor(Point predecessor) {
+	public void setPredecessor(Pathfinder predecessor) {
 		this.predecessor = predecessor;
 	}
 
@@ -29,6 +95,70 @@ public class Pathfinder {
 
 	public void setVisited(boolean visited) {
 		this.visited = visited;
+	}
+
+	public Pathfinder getNorth() {
+		return north;
+	}
+
+	public void setNorth(Pathfinder north) {
+		this.north = north;
+	}
+
+	public int getSpeedNorth() {
+		return speedNorth;
+	}
+
+	public void setSpeedNorth(int speedNorth) {
+		this.speedNorth = speedNorth;
+	}
+
+	public Pathfinder getEast() {
+		return east;
+	}
+
+	public void setEast(Pathfinder east) {
+		this.east = east;
+	}
+
+	public int getSpeedEast() {
+		return speedEast;
+	}
+
+	public void setSpeedEast(int speedEast) {
+		this.speedEast = speedEast;
+	}
+
+	public Pathfinder getSouth() {
+		return south;
+	}
+
+	public void setSouth(Pathfinder south) {
+		this.south = south;
+	}
+
+	public int getSpeedSouth() {
+		return speedSouth;
+	}
+
+	public void setSpeedSouth(int speedSouth) {
+		this.speedSouth = speedSouth;
+	}
+
+	public Pathfinder getWest() {
+		return west;
+	}
+
+	public void setWest(Pathfinder west) {
+		this.west = west;
+	}
+
+	public int getSpeedWest() {
+		return speedWest;
+	}
+
+	public void setSpeedWest(int speedWest) {
+		this.speedWest = speedWest;
 	}
 
 }
